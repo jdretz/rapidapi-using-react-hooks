822612bf3689159e3c5525fb3a6ce72ab43a2b50
Jarrett Retz <jarrett.retz@comcast.net>
2020-04-22 12:45:26 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  let [responseData, setResponseData] = React.useState('');
+
+  const fetchData = React.useCallback(() => {
+    axios({
+      "method": "GET",
+      "url": "https://quotes15.p.rapidapi.com/quotes/random/",
+      "headers": {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "quotes15.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY
+      }, "params": {
+        "language_code": "en"
+      }
+    })
+    .then((response) => {
+      setResponseData(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  React.useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Fetching Data with React Hooks
+        </h1>
+        <button type='button' onClick={fetchData}>Click for Data</button>
       </header>
+      <main>
+        {responseData &&
+          <blockquote>
+            "{responseData && responseData.content}"
+            <small>{responseData && responseData.originator && responseData.originator.name}</small>
+          </blockquote>
+        }
+        </main>
+      {/* <pre>
+        <code>
+          {responseData && JSON.stringify(responseData, null, 4)}
+        </code>
+      </pre> */}
     </div>
   );
 }
